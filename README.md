e88e6c3870d93d2b5886cf8069cd3dca0ed9be08
chardwillian <chardwillian9@gmail.com>
2021-04-19 16:48:21 -0300

    Aula 02
    Lidando com array

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 ## Aula 1 
 Aprendemos sobre alert, console log, variavéis, trocar variáveis com replace e ver os erros no console do navegador.
+
+## Aula 2
+Aprendemos como funciona o array, como inserir com o push e deletar com o pop, além de transformar em string com toString e colocar algo na string com o join
--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
+//Aula 01
+// var nome = "Chard";
+// var n1 = 21;
+// var n2 = 10;
+// var frase = "Japão é o melhor time do mundo"
+// //alert(nome + " tem " + idade + " anos");
+// //alert(idade + idade2);
 
-var nome = "Chard";
-var n1 = 21;
-var n2 = 10;
-var frase = "Japão é o melhor time do mundo"
-//alert(nome + " tem " + idade + " anos");
-//alert(idade + idade2);
+// console.log(nome);    //vai imprimir o nome
+// console.log(n1 * n2);
+// console.log(frase.toLocaleUpperCase()); //tudo maiusculo e LowerCase minusculo
+// console.log(frase.replace("Japão", "Brasil"));
+// //alert(frase.replace("Japão", "Brasil"));
 
-console.log(nome);
-console.log(n1 * n2);
-console.log(frase.toLocaleUpperCase()); //tudo maiusculo e LowerCase minusculo
-console.log(frase.replace("Japão", "Brasil"));
-//alert(frase.replace("Japão", "Brasil"));
+
+
+//Aula 02
+
+// var lista = [ "maça", "pêra", "laranja"];
+// lista.push("uva"); //adiciona uva na lista acima
+// lista.pop();  //pop remove o último elemento
+// console.log(lista);  //console.log(lista.reverse());  trás a lista ao contrário uva,laranja,pera
+//console.log(lista.toString());  vai transformar o objeto em string
+//console.log(lista.join(" - "))  vai imprimir a lista em forma de string mas separados por - 
+
+// var fruta = {nome: "maça", cor: "vermelha"}
+// console.log(fruta.nome);  //aparece o nome maça no console
+
+// var frutas = [{nome: "maça", cor: "vermelha"}, {nome: "uva", cor: "roxa"} ]
+// console.log(frutas);    //apresenta o array de frutas com nome e cor.
